080bfc7040f1d2af6ec8bd372416772c7ac166ff
davidleonstr <davidalfonsoleoncarmona@gmail.com>
2025-07-25 22:41:14 -0600

    Bug fixed in an example

--- examples/widget_display.py
+++ examples/widget_display.py
@@ -1,3 +1,4 @@
+import sys
 from qtpy.QtWidgets import QApplication, QMainWindow
 from QHubMarkdown import QHubMarkdown
 
@@ -5,26 +6,20 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        # Create an instance of QHubMarkdown with a dark theme (default theme)
-        self.markdown = QHubMarkdown(theme='light')
+        self.markdown = QHubMarkdown(theme='dark')
         
-        # Initial markdown content to display
         try:
             initialText = open('examples/markdown/markdown_example.md', encoding='utf-8').read()
         except:
             initialText = "# Hello 👋\nThis is a simple markdown renderer in Qt."
         
-        # Insert the markdown content into the widget
         self.markdown.insertMarkdown(initialText)
-        
-        # Set the widget as the central widget of the main window
         self.setCentralWidget(self.markdown)
-        
         self.setWindowTitle('Markdown Display')
         self.resize(800, 600)
         self.show()
 
 if __name__ == '__main__':
-    app = QApplication([])
+    app = QApplication(sys.argv)
     window = MainWindow()
-    app.exec_()
+    sys.exit(app.exec_())
